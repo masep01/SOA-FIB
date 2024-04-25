b25f005d7b891af4320c507e82651129babd82f6
masep01 <josepmg05@gmail.com>
2024-04-25 14:42:29 +0200

    restructured

---
+++ ZeOS_Entregues/interrupt.c
@@ -0,0 +1,153 @@
+/*
+ * interrupt.c -
+ */
+#include <types.h>
+#include <interrupt.h>
+#include <segment.h>
+#include <hardware.h>
+#include <io.h>
+
+#include <zeos_interrupt.h>
+
+Gate idt[IDT_ENTRIES];
+Register    idtR;
+
+int zeos_ticks = 0;
+extern struct task_struct * idle_task;
+
+char char_map[] =
+{
+  '\0','\0','1','2','3','4','5','6',
+  '7','8','9','0','\'','�','\0','\0',
+  'q','w','e','r','t','y','u','i',
+  'o','p','`','+','\0','\0','a','s',
+  'd','f','g','h','j','k','l','�',
+  '\0','�','\0','�','z','x','c','v',
+  'b','n','m',',','.','-','\0','*',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','7',
+  '8','9','-','4','5','6','+','1',
+  '2','3','0','\0','\0','\0','<','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0'
+};
+
+int strlen(char *a)
+{
+  int i;
+  i=0;
+  while (a[i]!=0) i++;
+  return i;
+}
+
+void print_hexa(unsigned long num){
+  char* digits = "0123456789ABCDEF";
+  for (int i = 28; i >= 0; i-=4)
+  {
+    printc(digits[(num>>i)&0xF]);
+  }
+}
+
+void setInterruptHandler(int vector, void (*handler)(), int maxAccessibleFromPL)
+{
+  /***********************************************************************/
+  /* THE INTERRUPTION GATE FLAGS:                          R1: pg. 5-11  */
+  /* ***************************                                         */
+  /* flags = x xx 0x110 000 ?????                                        */
+  /*         |  |  |                                                     */
+  /*         |  |   \ D = Size of gate: 1 = 32 bits; 0 = 16 bits         */
+  /*         |   \ DPL = Num. higher PL from which it is accessible      */
+  /*          \ P = Segment Present bit                                  */
+  /***********************************************************************/
+  Word flags = (Word)(maxAccessibleFromPL << 13);
+  flags |= 0x8E00;    /* P = 1, D = 1, Type = 1110 (Interrupt Gate) */
+
+  idt[vector].lowOffset       = lowWord((DWord)handler);
+  idt[vector].segmentSelector = __KERNEL_CS;
+  idt[vector].flags           = flags;
+  idt[vector].highOffset      = highWord((DWord)handler);
+}
+
+void setTrapHandler(int vector, void (*handler)(), int maxAccessibleFromPL)
+{
+  /***********************************************************************/
+  /* THE TRAP GATE FLAGS:                                  R1: pg. 5-11  */
+  /* ********************                                                */
+  /* flags = x xx 0x111 000 ?????                                        */
+  /*         |  |  |                                                     */
+  /*         |  |   \ D = Size of gate: 1 = 32 bits; 0 = 16 bits         */
+  /*         |   \ DPL = Num. higher PL from which it is accessible      */
+  /*          \ P = Segment Present bit                                  */
+  /***********************************************************************/
+  Word flags = (Word)(maxAccessibleFromPL << 13);
+
+  //flags |= 0x8F00;    /* P = 1, D = 1, Type = 1111 (Trap Gate) */
+  /* Changed to 0x8e00 to convert it to an 'interrupt gate' and so
+     the system calls will be thread-safe. */
+  flags |= 0x8E00;    /* P = 1, D = 1, Type = 1110 (Interrupt Gate) */
+
+  idt[vector].lowOffset       = lowWord((DWord)handler);
+  idt[vector].segmentSelector = __KERNEL_CS;
+  idt[vector].flags           = flags;
+  idt[vector].highOffset      = highWord((DWord)handler);
+}
+
+/* Handlers */
+void clock_handler();
+void keyboard_handler();
+void my_page_fault_handler();
+void syscall_handler_sysenter();
+
+/* Service routines */
+void clock_routine(){
+  zeos_ticks++;
+  zeos_show_clock();
+  schedule();
+}
+
+/* Others */
+writeMSR(unsigned long msr, unsigned long addr);
+
+void keyboard_routine(){
+  unsigned char reg = inb(0x60);         // Read 0x60 data register
+  int its_make = reg & 0x80;             // Distinguish a make from a break (press or release)
+                                         // Mask = 1000 0000 = 0x80
+  if(its_make){
+    char c = char_map[reg & 0x7f];       // Extract the pressed character. Mask = 0111 1111 = 0x7F
+    if(c <= 32 || 127 <= c) c = 'C';     // If key does not match any ASCII character, print 'C'
+    printc_xy(75,20,c);
+  }
+}
+
+void my_page_fault_routine(unsigned long param, unsigned long eip){
+  char* msg = "\nProcess generates a PAGE FAULT exception at EIP: ";
+
+  /* Print message */
+  printk(msg);
+  printk("0x");
+  print_hexa(eip);
+
+  while(1){}
+}
+
+void setIdt()
+{
+  /* Program interrups/exception service routines */
+  idtR.base  = (DWord)idt;
+  idtR.limit = IDT_ENTRIES * sizeof(Gate) - 1;
+  
+  set_handlers();
+
+  /* ADD INITIALIZATION CODE FOR INTERRUPT VECTOR */
+  setInterruptHandler(32, clock_handler, 0);
+  setInterruptHandler(33, keyboard_handler, 0);
+  setInterruptHandler(14, my_page_fault_handler, 0);
+
+  /* Write MSR */
+  writeMSR(0x174, __KERNEL_CS);
+  writeMSR(0x175, INITIAL_ESP);
+  writeMSR(0x176, (unsigned long)syscall_handler_sysenter);
+
+  set_idt_reg(&idtR);
+}
+
