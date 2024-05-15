e59759d6e146711302ea750f2066ba670774a54a
masep01 <josepmg05@gmail.com>
2024-05-15 16:23:02 +0200

    game started

---
+++ GAME/include/game.h
@@ -0,0 +1,24 @@
+#ifndef _LINUX_GAME_H
+#define _LINUX_GAME_H
+
+#define TICS_SEC 500
+
+#define SCREEN_ROWS 25
+#define SCREEN_COLS 80
+
+#define START_MAP_X 10
+#define START_MAP_Y 5
+
+#define END_MAP_X (SCREEN_COLS - START_MAP_X)
+#define END_MAP_Y (SCREEN_ROWS - START_MAP_Y)
+
+#define INITIAL_PLAYER_X 40
+#define INITIAL_PLAYER_Y 15
+
+void calc_fps();
+void clean_screen();
+void print_map();
+void init_game();
+void set_and_print_player();
+
+#endif
